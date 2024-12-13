ed1298a62cb572c8c7f1d7f09966a53da9373948
amadousadialloh <amadousadialloh@gmail.com>
2024-12-12 23:43:29 -0600

    complete user component

--- mern-frontend/src/components/UserComponents/login/login.jsx
+++ mern-frontend/src/components/UserComponents/login/login.jsx
@@ -1,2 +1,59 @@
+const Login = ({ userInputs, setUserInputs }) => {
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    const newUserInputs = { ...userInputs, [name]: value };
+    setUserInputs(newUserInputs);
+  };
 
-// Pass a login 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: userInputs.username,
+          password: userInputs.password,
+        }),
+      });
+
+      const JSONdata = await res.json();
+      console.log(JSONdata);
+    } catch (error) {
+      console.error("Error logging in", error);
+    }
+  };
+
+  return (
+    <>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <p>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={userInputs.username}
+            onChange={handleInputChange}
+            required
+          />
+        </p>
+        <p>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={userInputs.password}
+            onChange={handleInputChange}
+            required
+          />
+        </p>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
--- mern-frontend/src/components/UserComponents/signup/signup.jsx
+++ mern-frontend/src/components/UserComponents/signup/signup.jsx
@@ -1,1 +1,70 @@
-//do we want front end validation?
+const SignUp = ({ userInputs, setUserInputs }) => {
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserInputs({ ...userInputs, [name]: value });
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: userInputs.username,
+          password: userInputs.password,
+          confirmPassword: userInputs.confirmPassword,
+        }),
+      });
+
+      const JSONdata = await res.json();
+      console.log(JSONdata);
+    } catch (error) {
+      console.error("Error signing up", error);
+    }
+  };
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignup}>
+        <p>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={userInputs.username || ""}
+            onChange={handleInputChange}
+            required
+          />
+        </p>
+        <p>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={userInputs.password || ""}
+            onChange={handleInputChange}
+            required
+          />
+        </p>
+        <p>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            value={userInputs.confirmPassword || ""}
+            onChange={handleInputChange}
+            required
+          />
+        </p>
+        <button type="submit">Sign Up</button>
+      </form>
+    </>
+  );
+};
+
+export default SignUp;

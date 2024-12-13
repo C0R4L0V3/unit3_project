af42c5ae3be7f8bd2d5fde8e88523167939d2e3e
ChrisL <caleedev@gmail.com>
2024-12-13 16:47:21 -0500

    debugged Home

--- mern-frontend/src/App.jsx
+++ mern-frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar.jsx";
-// import Home from './components/home/Home.jsx'
+import Home from './components/home/Home.jsx'
 // import Header from './components/header/Header.jsx'
 import Footer from "./components/footer/Footer.jsx";
+import Login from "./components/UserComponents/login/login.jsx";
+import Signup from "./components/UserComponents/signup/signup.jsx";
+import Profile from "./components/UserComponents/profile/profile.jsx";
 
-const formData = function App() {
+const App = () => {
+// function App() {
   const [page, setPage] = useState("Home");
   const [content, setContent] = useState([]);
   const [user, setUser] = useState(null);
-  const [formdata, setFormData] = useState({
+  const [formData, setFormData] = useState({
     username: "",
     password: "",
     confirmPassword: "",
@@ -17,24 +21,26 @@ const formData = function App() {
 
   const handleNav = (page) => {
     console.log(page);
+    setPage(page);
     if (page === "Logout") setUser(null); // this should log out the user
   };
 
   return (
     <>
-      {/*     
-      {page === 'Landing' ? <Landing /> : ''}
-      {page === 'Signup' ? <Signup user={user} setUser={setUser}/> : ''}
-      {page === 'Login' ? <Login  user={user} setUser={setUser}/> : ''} */}
-      {/* {page === 'Home' ? <Home content={content} setContent={setContent}/> : ''} */}
+         
+      {/* {page === 'Landing' ? <Landing /> : ''} */}
+      {page === 'Signup' ? <Signup formData={formData} setFormData={setFormData} setUser={setUser}/> : ''}
+      {page === 'Login' ? <Login  formData={formData} setFormData={setFormData} setUser={setUser} setPage={setPage}/> : ''} 
+      {page === 'Home' ? <Home content={content} setContent={setContent}/> : ''}
       {/* {page === 'Upload' ? <Upload content={content} setContent={setContent}/> : ''} */}
-      {/* {page == 'Profile' ? <Profile content={content} setContent={setContent}/> : ''} */}
+      {page == 'Profile' ? <Profile content={content} setContent={setContent}/> : ''}
       {/* <Header /> */}
       <Navbar handleNav={handleNav} user={user} />{" "}
       {/* passing handler to set page state // passing user state to determine how the nave bar looks*/}
       <Footer />
     </>
   );
+// };
 };
 
 export default App;
--- mern-frontend/src/components/home/Home.jsx
+++ mern-frontend/src/components/home/Home.jsx
@@ -5,10 +5,10 @@ const Home = ({content, setContent}) => {
         useEffect(() => {
             const fetchContent = async () => {
                 try{
-                    let res = await fetch(`${import.meta.env.VITE_API_URL}/index`);//gets content from all users
+                    let res = await fetch(`${import.meta.env.VITE_API_URL}/users/index`);//gets content from all users
                     let JSONdata = await res.json()
                     console.log(JSONdata);
-                    setContent(JSONdata)
+                    setContent(JSONdata.displayContent)
                     
                 } catch (error) {
                     console.error('Error fetching data');
@@ -23,22 +23,25 @@ const Home = ({content, setContent}) => {
         <>
             <h1>Home Page!</h1>
             <div className="contentContainer">
-                {content.length ? ( //Ternary Wrapper in case we have no content on the home page.
-                content.map((post) => (//Map through fetchContent()
+                
                 <ul>
-                     <li className = "postContainer" key={post._id}> {/* display posts. */}
+                {content.length ? ( //Ternary Wrapper in case we have no content on the home page.
+                content.map((post, index) => (//Map through fetchContent()
+                     <li className = "postContainer" key={index}> {/* display posts. */}
                         <h3>Name: {post.name}</h3>
                         <p>{post.value}</p>
                         <p>{post.dateUploaded}</p>
 
                     </li>
+                    ))
+            
+                ) : (
+                    <p>Sorry something went wrong.  Fire the Devs.</p>
+                )}
+                
                 </ul>
 
-                ))
-            
-        ) : (
-            <p>Sorry something went wrong.  Fire the Devs.</p>
-        )}
+                
             </div>
         </>
 

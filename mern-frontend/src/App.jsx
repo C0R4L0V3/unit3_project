df428eb1729d7f57903945b61bb09a3d4d8c507b
C0R4L0V3 <coraline.e.love@gmail.com>
2024-12-17 08:42:49 -0600

    updated function initialized

--- mern-frontend/src/App.jsx
+++ mern-frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./components/UserComponents/login/login.jsx";
 import Signup from "./components/UserComponents/signup/signup.jsx";
 import Profile from "./components/UserComponents/profile/profile.jsx";
 import Upload from "./components/upload/UploadFile.jsx"
+import Update from "./components/Update/Update.jsx";
 
 const App = () => {
 // function App() {
@@ -36,7 +37,8 @@ const App = () => {
       {page === 'Login' ? <Login  user={user} formData={formData} setFormData={setFormData} setUser={setUser} setPage={setPage}/> : ''} 
       {page === 'Home' ? <Home content={content} setContent={setContent}/> : ''}
       {page === 'Upload' ? <Upload user={user} setUser={setUser} setPage={setPage}/> : ''}
-      {page == 'Profile' ? <Profile user={user}/> : ''}
+      {page === 'Profile' ? <Profile user={user}/> : ''}
+      {page === 'Update' ? <Update user={user} setUser={setUser} setPage={setPage} /> : ''}
       {/* <Header /> */}
       {/* passing handler to set page state // passing user state to determine how the nave bar looks*/}
       <Footer />
---
+++ mern-frontend/src/components/Update/Update.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './update.css'
+
+const Update = (props) => {
+    const [file, setFile] = useState ({
+        //model keys
+    })
+
+
+    return (
+
+    )
+}
+
+export default Update
